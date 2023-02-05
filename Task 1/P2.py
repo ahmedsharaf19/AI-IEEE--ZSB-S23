e8847707c453ff5db19d82327289d62a159f1fcb
Ahmed Sharaf <113258822+ahmedsharaf19@users.noreply.github.com>
2023-02-05 23:01:07 +0200

    Update P2.py

--- Task 1/P2.py
+++ Task 1/P2.py
@@ -1,8 +1,10 @@
-x = input()
+x = [[[10,7,50],[16,20,16],[50,7,10]]]
 flag = True
 for i in range(len(x)) :
-    if x[i] != x[-i-1] : 
-        flag = False
-        break
+    for j in range(len(x)):
+        if x[i][j] != x[j][i] : 
+            flag= False
+            break
+    if flag == False : break
 
 print(flag)
