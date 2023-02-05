6623fad42e4675a08c7d007901169be2f65dba35
Ahmed Sharaf <113258822+ahmedsharaf19@users.noreply.github.com>
2023-02-05 14:28:27 +0200

    Update P6.py

--- Task 1/P6.py
+++ Task 1/P6.py
@@ -1,25 +1,15 @@
-dic = {}
-list1=[]
-for n in input().split(",") :
-    list1 += n.split(":")
+x = input().split(",")
+list =[]
+for n in x :
+    list.append(n.split(":"))
 
-list1_key = list1[::2]
-list1_value = list1[1::2]
+x = input().split(",")
+for n in x :
+    list.append(n.split(":"))
 
-for key in list1_key:
-    for value in list1_value:
-        dic[key] = value
-        list1_value.remove(value)
-        break
-list2=[]
-for n in input().split(",") :
-    list2 += n.split(":")
+list = dict(list)
+dict = {}
+for main_key , main_value in list.items() :
+    dict.setdefault(main_key,int(main_value)) 
 
-list2_key = list2[::2]
-list2_value = list2[1::2]
-for key in list2_key:
-    for value in list2_value:
-        dic[key] = value
-        list2_value.remove(value)
-        break
-print(dic)
+print(dict)
