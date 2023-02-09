f04953dac4c1b230a13a95df80663c60ee857d93
Ahmed Sharaf <113258822+ahmedsharaf19@users.noreply.github.com>
2023-02-09 15:10:35 +0200

    Create P3.py

---
+++ Task 2/P3.py
@@ -0,0 +1,37 @@
+def max_subarray(array): 
+    array_sorted=sorted(array)
+    reversed_array=array_sorted[::-1]
+    sub_max=[reversed_array[0]]
+    result=reversed_array[0]
+    for i in range(1,len(reversed_array)):
+        buffer=result+ reversed_array[i]
+        if(result <= buffer or len(sub_max)== 1):
+            sub_max.append(reversed_array[i])
+            result=buffer
+        else:
+            break
+    
+    return sub_max,result
+
+def smallest_sum(array): 
+    array_sorted=sorted(array)
+    sub_mim=[array_sorted[0]]
+    result=array_sorted[0]
+    for i in range(1,len(array_sorted)):
+        buffer=result+ array_sorted[i]
+        if(result >= buffer or len(sub_mim)== 1):
+            sub_mim.append(array_sorted[i])
+            result=buffer
+        else:
+            break
+    return sub_mim,result
+
+
+
+array= [5 ,4 ,-1, 7, 8]
+
+max_ar,result_max_sub=max_subarray(array)
+print(f"{max_ar} ({result_max_sub})")
+
+min_ar,result_min_sub=smallest_sum(array)
+print(f"{min_ar} ({result_min_sub})")
